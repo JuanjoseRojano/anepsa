2ef7da04d0770b593ace8048802825473896e7d9
JuanjoseRojano <tu-email-asociado-a-JuanjoseRojano>
2025-03-27 16:58:07 +0100

    Primer archivo readme

--- src/components/componenteFooter.jsx
+++ src/components/componenteFooter.jsx
@@ -3,7 +3,7 @@ import Logo from '../media/logo.png'
 
 function ComponenteFooter() {
     return (
-        <section className="py-10 bg-gray-50 sm:pt-16 lg:pt-24 position:absolute bottom-0 mt-auto   ">
+        <section className="py-10 bg-gray-50 sm:pt-16 lg:pt-24  bottom-0 mt-auto   ">
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                 <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
                     <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
