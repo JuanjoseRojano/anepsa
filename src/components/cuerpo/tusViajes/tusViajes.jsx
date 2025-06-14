b6855a1f0eccb82f49515b883dd22bd9d8c80a9e
JuanjoseRojano <tu-email-asociado-a-JuanjoseRojano>
2025-06-14 19:44:42 +0200

    problema encontrado a ultima hora

--- src/components/cuerpo/tusViajes/tusViajes.jsx
+++ src/components/cuerpo/tusViajes/tusViajes.jsx
@@ -1,102 +1,110 @@
-import { useState } from 'react'
-import { BrowserRouter as Link, Navigate } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { Link, Navigate } from "react-router-dom"
 import TarjetasMisViajesEditable from './tarjetasMisViajesEditable'
 
 import { isBefore } from 'date-fns'
 
 function TusViajes(props) {
 
+    const [contenidoTusViajes, setContenidoTusViajes] = useState(null)
+
+    useEffect(() => {
+        separarTarjetas()
+    }, [])
+
+    useEffect(() => {
+        separarTarjetas()
+    }, [props.usuarioConectado])
+
     //funcion que separa los viajes editables de los no editables
-    const separarTarjetas = (element) => {
+    const separarTarjetas = () => {
 
-
-        const fechaDeVueloTransformada = new Date(element.fechaDeVuelo)
-
-        const fechaActualSumar2 = new Date()
-        const fechaLimite = new Date()
-
-
-        const fechaActualMas2 = new Date(fechaActualSumar2.setDate(fechaActualSumar2.getDate() + 2))
-        const fechaActualLimite = new Date(fechaLimite.setDate(fechaLimite.getDate() - 1))
-
-
-        let renderizarBilleteEditableONoEditable
-
-        if (isBefore(fechaDeVueloTransformada, fechaActualLimite)) {
-            renderizarBilleteEditableONoEditable = 2
-
-        } else if (isBefore(fechaDeVueloTransformada, fechaActualMas2)) {
-            renderizarBilleteEditableONoEditable = 1
-
-        }
+        if (props.usuarioConectado === null) (
+            setContenidoTusViajes(<Navigate to='../Sesion'></Navigate>)
+        )
         else {
-            renderizarBilleteEditableONoEditable = 0
+            const listaComponentesEditables = []
 
+
+            props.usuarioConectado.viajes.forEach(element => {
+                let renderizarBilleteEditableONoEditable
+
+                const fechaDeVueloTransformada = new Date(element.fechaDeVuelo)
+
+                const fechaActualSumar2 = new Date()
+                const fechaLimite = new Date()
+
+
+                const fechaActualMas2 = new Date(fechaActualSumar2.setDate(fechaActualSumar2.getDate() + 2))
+                const fechaActualLimite = new Date(fechaLimite.setDate(fechaLimite.getDate() - 1))
+
+
+
+                if (isBefore(fechaDeVueloTransformada, fechaActualLimite)) {
+                    renderizarBilleteEditableONoEditable = 2
+
+                } else if (isBefore(fechaDeVueloTransformada, fechaActualMas2)) {
+                    renderizarBilleteEditableONoEditable = 1
+
+                }
+                else {
+                    renderizarBilleteEditableONoEditable = 0
+
+                }
+
+
+
+                switch (renderizarBilleteEditableONoEditable) {
+
+                    case 0:
+
+                        listaComponentesEditables.push(<><div className="flex justify-center items-center min-h-screen p-3">
+
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4  justify-center p-3">
+                                <TarjetasMisViajesEditable
+                                    element={element}
+                                    usuarioConectado={props.usuarioConectado}
+                                    setUsuarioConectado={props.setUsuarioConectado}
+                                    usuarios={props.Usuarios}
+                                    setUsuarios={props.setUsuarios}
+                                    viajes={props.viajes}
+                                    setViajes={props.setViajes}
+                                    setError={props.setError}
+                                />
+                            </div>
+                        </div></>)
+
+                        break
+                    //Estos otros case los decidí mantener para mostrar algo diferente, sin embargo mantengo la duda de si es buena idea
+                    case 1:
+
+                        break
+                    case 2:
+                        break
+                }
+            })
+            console.log(listaComponentesEditables)
+            if (listaComponentesEditables.length === 0 || props.usuarioConectado.viajes.length === 0) {
+                setContenidoTusViajes(<div className="flex flex-col items-center mt-6">
+                    <h2 className="text-xl text-gray-600">Sin viajes posibles de actualizar, pulsa <span className='text-blue-400 hover:font-bold hover:text-blue-500'><Link to="/ReservarViajes">Aquí</Link></span> para comprar alguno</h2>
+                    <img
+                        src="https://blog.selfbank.es/wp-content/uploads/2018/03/iStock-584759976.jpg"
+                        alt="Sin viajes"
+                        className="mt-4 w-96 h-auto rounded-lg shadow-md"
+                    />
+                </div>)
+            }
+            else {
+                setContenidoTusViajes(listaComponentesEditables)
+            }
         }
-
-
-
-        switch (renderizarBilleteEditableONoEditable) {
-
-            case 0:
-                return (<TarjetasMisViajesEditable
-                    element={element}
-                    usuarioConectado={props.usuarioConectado}
-                    setUsuarioConectado={props.setUsuarioConectado}
-                    usuarios={props.suarios}
-                    setUsuarios={props.setUsuarios}
-                    viajes={props.viajes}
-                    setViajes={props.setViajes}
-                    setError={props.setError}
-                />)
-                break
-            //Estos otros case los decidí mantener para mostrar algo diferente, sin embargo mantengo la duda de si es buena idea
-            case 1:
-
-                break
-            case 2:
-                break
-        }
-
-
-
     }
 
-    //Redirige al usuario a /Sesion en caso de que el usuario no este registrado
-    if (props.usuarioConectado === null) {
-        return (<Navigate to='../Sesion'></Navigate>)
-    }
-    else if (props.usuarioConectado.viajes.length === 0) {
-
-        return (
-            <div className="flex flex-col items-center mt-6">
-                <h2 className="text-xl text-gray-600">Sin viajes en espera pulsa <span className='text-blue-400 hover:font-bold hover:text-blue-500'><Link to="/ReservarViajes">Aquí</Link></span> para comprar alguno</h2>
-                <img
-                    src="https://blog.selfbank.es/wp-content/uploads/2018/03/iStock-584759976.jpg"
-                    alt="Sin viajes"
-                    className="mt-4 w-96 h-auto rounded-lg shadow-md"
-                />
-            </div>)
-
-    }
-    else {
-
-        return (<>
-            <div className="flex justify-center items-center min-h-screen p-3">
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4  justify-center p-3">
-                    {
-                        props.usuarioConectado.viajes.map((element) => (
-                            separarTarjetas(element)
-
-                        ))
-                    }
-                </div>
-            </div></>)
-
-    }
-
-
+    return (
+        <>
+            {contenidoTusViajes}
+        </>
+    )
 }
 
 export default TusViajes
